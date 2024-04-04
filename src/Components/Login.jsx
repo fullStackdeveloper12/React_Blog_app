5c38ba62bfe6f1525317f3a84c4221531d211b04
itsmeshubham11 <shubhamsharma1111234@gmail.com>
2024-04-04 21:19:59 +0530

    first commit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,39 +1,34 @@
-import React from 'react'
-import { auth } from '../Firebase'
-import {GoogleAuthProvider,signInWithPopup} from "firebase/auth"
-import { useNavigate } from 'react-router-dom';
-
+import React from "react";
+import { auth } from "../Firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
 
-  const navigate=useNavigate();
-
-  const googleClick= async()=>{
-    const provider=new GoogleAuthProvider();
-    const result= await signInWithPopup(auth,provider);
+  const googleClick = async () => {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
     console.log(result);
-    navigate("/Blogs")
-  }
-
-
-
+    navigate("/Blogs");
+  };
 
   return (
     <>
-    
-    
-    
-    <div className="container">
-      <h1>LET'S CREATE</h1>
-      <div className='img-container'>
-        <img 
-        onClick={googleClick}
-        src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"  style={{width:"10%"}} alt="Google-login" />
-        <span onClick={googleClick}>Sign In With Google</span>
+      <div className="container">
+        <h1>LET'S CREATE</h1>
+        <div className="img-container">
+          <img
+            onClick={googleClick}
+            src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
+            style={{ width: "10%" }}
+            alt="Google-login"
+          />
+          <span onClick={googleClick}>Sign In With Google</span>
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
